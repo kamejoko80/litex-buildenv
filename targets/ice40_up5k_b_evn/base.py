16cda35c8f12ab1e94e0d61d2e9d6b66f5dc1803
kamejoko80 <phuongminh.dang@gmail.com>
2019-06-14 00:24:55 +0700

    Added 40CE PLL

--- targets/ice40_up5k_b_evn/base.py
+++ targets/ice40_up5k_b_evn/base.py
@@ -40,20 +40,20 @@ class _CRG(Module):
         # FIXME: Use PLL, increase system clock to 32 MHz, pending nextpnr
         # fixes.
         # Fout = Fin x (DIVF + 1) / (2^DIVQ x (DIVR + 1))
-        # self.specials += \
-        #    Instance("SB_PLL40_PAD",
-        #        p_FEEDBACK_PATH="SIMPLE",
-        #        p_DIVR=0,
-        #        p_DIVF=0,
-        #        p_DIVQ=0,
-        #        p_FILTER_RANGE=1,
-        #        i_RESETB=1,
-        #        i_BYPASS=0,                
-        #        i_PACKAGEPIN=clk12,
-        #        o_PLLOUTCORE=self.cd_sys.clk,
-        #    )
-        
-        self.comb += self.cd_sys.clk.eq(clk12)
+        self.specials += \
+            Instance("SB_PLL40_2_PAD",
+                p_FEEDBACK_PATH="SIMPLE",
+                p_DIVR=0b0000,    # 0
+                p_DIVF=0b1111,    # 15
+                p_DIVQ=0b100,     # 4
+                p_FILTER_RANGE=0b010,
+                i_RESETB=1,
+                i_BYPASS=0,                
+                i_PACKAGEPIN=clk12,
+                o_PLLOUTGLOBALA=self.cd_sys.clk,
+            )
+
+        # self.comb += self.cd_sys.clk.eq(clk12)
         
         # POR reset logic- POR generated from sys clk, POR logic feeds sys clk
         # reset.
